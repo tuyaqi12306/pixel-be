7d28b7da7faf3e8940a5e869470e826d9d08adb9
tuyaqi12306 <1055126409@qq.com>
2019-12-03 22:29:25 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const express = require('express')
+const SocketIO = require('socket.io')
+
+const app = express()
+const port = 3005
+const server = app.linsten(port, () => {
+
+})
+const io = SocketIO(server)
+// app.use(express.static(''))
+const pixelData = [
+  ['red','blue','green'],
+  ['red','blue','green'],
+  ['red','blue','green'],
+  ['red','blue','green']
+]
+io.on('connection', (ws) => { // 服务器连接
+  ws.emit('pixel-data', pixelData)
+  ws.on('disconnect', () => { // 关闭连接
+    console.log('someone leaves')
+  })
+})
